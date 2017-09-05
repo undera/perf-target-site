3ecd841f77a012cd0a0910367ab17d9fcfe22ac7
Andrey Pokhilko <apc4@ya.ru>
2017-09-04 20:03:28 -0700

    Add flapping RT

--- Demo/RTimeFlapping.php
+++ Demo/RTimeFlapping.php
@@ -7,13 +7,21 @@ use PWE\Core\PWELogger;
 use PWE\Modules\Outputable;
 use PWE\Modules\PWEModule;
 
-class RTimeConstant extends PWEModule implements Outputable
+class RTimeFlapping extends PWEModule implements Outputable
 {
     public function process()
     {
-        $duration = intval($_REQUEST['delay']);
+        $relaxed = intval($_REQUEST['relaxed']);
+        $stuck = intval($_REQUEST['stuck']);
+        if ((time() % ($relaxed + $stuck)) > $relaxed) {
+            $duration = intval($_REQUEST['delay_stuck']);
+        } else {
+            $duration = intval($_REQUEST['delay_relaxed']);
+        }
+
+
         PWELogger::info("Sleeping for $duration");
-        usleep($duration*1000);
+        usleep($duration * 1000);
 
         $smarty = $this->PWE->getSmarty();
         $smarty->setTemplateFile(__DIR__ . '/text.tpl');
